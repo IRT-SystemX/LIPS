acf72f44bae8015e7d303eef00de6758de5ae7ad
Mleyliabadi <mleyliabadi@gmail.com>
2021-08-26 23:43:02 +0200

    the hyper parameter tunining utility is removed from this class

--- lips/augmented_simulators/fullyConnectedAS.py
+++ lips/augmented_simulators/fullyConnectedAS.py
@@ -294,44 +294,5 @@ class FullyConnectedAS(AugmentedSimulator):
 
         return res_x, res_y
 
-    def hyperparameter_tuning(self, dataset, sizes_layer=[(150,150)], layer_act=["relu"], lr=[3e-4], batch_size=[128], epochs=[5], loss=["mse"],n_folds=3, verbose=1):
-        """
-        This function allows to run a grid search cross validation over a set of hyper parameters
-        
-        All the parameters of this function except for dataset, epochs and number of folds could be a list
-        """
-        processed_x, processed_y = self._process_all_dataset(dataset, training=True)
-
-        from sklearn.model_selection import GridSearchCV
-        from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
-
-        model = KerasRegressor(build_fn=self.__create_model, verbose=0)
-
-        param_grid = dict(sizes_layer=sizes_layer, layer_act=layer_act, lr=lr, batch_size=batch_size, epochs=epochs, loss=loss)
-
-        grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=n_folds)
-
-        grid_result = grid.fit(processed_x, processed_y, verbose=verbose)
-
-        print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-        means = grid_result.cv_results_['mean_test_score']
-        stds = grid_result.cv_results_['std_test_score']
-        params = grid_result.cv_results_['params']
-        for mean, stdev, param in zip(means, stds, params):
-            print("%f (%f) with: %r" % (mean, stdev, param))
-
-        return grid_result
-
-
-    def __create_model(self, sizes_layer=(150,150), layer_act="relu", lr=3e-4, loss="mse"):
-        self.sizes_layer = sizes_layer
-        self.layer_act = layer_act
-        
-        self.init()
-        optimizer = tfko.Adam(learning_rate=lr)
-        self._model.compile(optimizer=optimizer, loss=loss, metrics=["mae"])
-
-        return self._model
-
 
 
