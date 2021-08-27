543b861f5d75be82cf3f754969888e8eb33d64f6
Mleyliabadi <mleyliabadi@gmail.com>
2021-08-27 02:17:56 +0200

    Physics compliance verifications are updated
    Former-commit-id: fcb2f4c5640f3520279c926749e86b47b8b8abdf
    Former-commit-id: 5593d569476cc7b8e6ef5303106979e9ca4e8c9b
    Former-commit-id: 446cda729d3416067f6b3ccf25564ee68708d3c5

--- lips/evaluation/Check_energy_conservation.py
+++ lips/evaluation/Check_energy_conservation.py
@@ -1,5 +1,13 @@
+# Copyright (c) 2021, IRT SystemX (https://www.irt-systemx.fr/en/)
+# See AUTHORS.txt
+# This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0.
+# If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
+# you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+# This file is part of LIPS, LIPS is a python platform for power networks benchmarking
+
 import numpy as np
-import os
+from sklearn.metrics import mean_absolute_error
 
 
 def Check_energy_conservation(prod_p=None, load_p=None, p_or=None, p_ex=None, tolerance=1e-3):
@@ -39,12 +47,16 @@ def Check_energy_conservation(prod_p=None, load_p=None, p_or=None, p_ex=None, to
 
     productions = np.sum(prod_p, axis=1)
     loads = np.sum(load_p, axis=1)
-    loss = np.sum(p_or + p_ex, axis=1)
+    loss = np.sum(p_or + p_ex, axis=1) # TODO : not np.abs(np.sum(...)) ? Try and see
 
     LCE = productions - loads - loss
     failed_indices = np.array(np.where(abs(LCE) > tolerance)).reshape(-1, 1)
     violation_percentage = (len(failed_indices) / len(LCE))*100
-    print("Number of failed cases is {} and the proportion is {:.3f}% : ".format(
-        len(failed_indices), violation_percentage))
+    #print("Number of failed cases is {} and the proportion is {:.3f}% : ".format(
+    #    len(failed_indices), violation_percentage))
 
-    return LCE, violation_percentage, failed_indices
+    criteria = mean_absolute_error(loads - productions, loss)
+
+    print("Mean Absolute Error (MAE) between (loads - productions) and loss is : {:.3f}", criteria)
+
+    return LCE, violation_percentage, failed_indices, criteria
