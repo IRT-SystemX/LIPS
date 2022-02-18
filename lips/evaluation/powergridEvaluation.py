8f7ae03c5e34a505c097fe425bcc016546190d58
Mleyliabadi <mleyliabadi@gmail.com>
2022-02-18 22:13:14 +0100

    New Evaluation module structure initiated

---
+++ lips/evaluation/powergridEvaluation.py
@@ -0,0 +1,17 @@
+from lips.Evaluation import Evaluation
+
+class PowerGridEvaluation(Evaluation):
+    """
+    This class allows the evaluation of augmented simulators applied to power grids cases
+    """
+
+    def __init__(self):
+        pass
+
+    def evaluate_Physics(self):
+        pass
+
+    def evaluate_ML(self):
+        pass
+
+    
