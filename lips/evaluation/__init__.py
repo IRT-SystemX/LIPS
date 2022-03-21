9057d10fd83b52aa069b1a657019e5aea63b6c8d
mleyliabadi <mleyliabadi@gmail.com>
2022-03-21 23:55:51 +0100

    evaluation module refactoring initiated

--- lips/evaluation/__init__.py
+++ lips/evaluation/__init__.py
@@ -1,23 +1,24 @@
-# Copyright (c) 2021, IRT SystemX (https://www.irt-systemx.fr/en/)
-# See AUTHORS.txt
-# This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0.
-# If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
-# you can obtain one at http://mozilla.org/MPL/2.0/.
-# SPDX-License-Identifier: MPL-2.0
-# This file is part of LIPS, LIPS is a python platform for power networks benchmarking
+"""
+Usage:
+    /__init__.py
+Licence:
+    Copyright (c) 2021, IRT SystemX (https://www.irt-systemx.fr/en/)
+    See AUTHORS.txt
+    This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0.
+    If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
+    you can obtain one at http://mozilla.org/MPL/2.0/.
+    SPDX-License-Identifier: MPL-2.0
+    This file is part of LIPS, LIPS is a python platform for power networks benchmarking
+"""
 
-__all__ = ["Evaluation", "Check_loss", "BasicVerifier",
-           "Check_energy_conservation", "check_kcl"]
+from __future__ import absolute_import
 
+from .evaluation import Evaluation
+from .powergrid_evaluation import PowerGridEvaluation
+from .utils import Mapper
 
-from lips.evaluation.Evaluation import Evaluation
-from lips.evaluation.Check_loss import Check_loss
-from lips.evaluation.BasicVerifier import BasicVerifier
-from lips.evaluation.Check_energy_conservation import Check_energy_conservation
-from lips.evaluation.Check_KCL import check_kcl
-try:
-    from lips.evaluation.Check_Kirchhoff_current_law import Check_Kirchhoff_current_law
-    __all__.append("Check_Kirchhoff_current_law")
-except ImportError:
-    # grid2Op is not installed
-    pass
+__all__ = [
+    "Evaluation",
+    "PowerGridEvaluation",
+    "Mapper"
+]
