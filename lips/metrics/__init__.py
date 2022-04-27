fa9e64fbbea7f11b683d428aab06b77c87254ea5
mleyliabadi <mleyliabadi@gmail.com>
2022-04-27 18:29:54 +0200

    This closes #68

--- lips/metrics/__init__.py
+++ lips/metrics/__init__.py
@@ -11,7 +11,20 @@ Licence:
     This file is part of LIPS, LIPS is a python platform for power networks benchmarking
 """
 
-__all__ = ["mape_quantile", "nrmse", "mape", "pearson_r", "DEFAULT_METRICS"]
+#__all__ = ["mape_quantile", "nrmse", "mape", "pearson_r", "DEFAULT_METRICS"]
 
-from lips.metrics.ml_metrics import mape_quantile, nrmse, mape, pearson_r
-from lips.metrics.ml_metrics import DEFAULT_METRICS
+#from lips.metrics.ml_metrics import mape_quantile, nrmse, mape, pearson_r
+#from lips.metrics.ml_metrics import DEFAULT_METRICS
+
+__all__ = ["DEFAULT_METRICS"]
+
+try:
+    from leap_net.metrics import nrmse
+    from leap_net.metrics import mape
+    from leap_net.metrics import mape_quantile
+    from leap_net.metrics import pearson_r
+    from leap_net.proxy.utils import DEFAULT_METRICS
+    __all__ += ["nrmse", "mape", "mape_quantile", "pearson_r"]
+except ImportError as exc_:
+    # leap net package not available
+    DEFAULT_METRICS = {}
